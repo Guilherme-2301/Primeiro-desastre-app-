47022ccb7bb581f82d303cfd8a5f2ea8f859875f
GUILHERME DA SILVA NEVES <guilherme_da-neves@estudante.sesisenai.org.br>
2025-09-17 17:10:04 -0300

    17-09-25

--- App.js
+++ App.js
@@ -1,14 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, TextInput , TouchableOpacity,SafeAreaView,ScrollView} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [view, setView] = useState('lista');
+  const[recipes,setRecipes]=useState([]);
+  const[title,setTitle]=useState("");
+  const[igredients,setIgredients]=useState("");
+ useEffect(()=>{
+  const loadRecepies=async()=>{
+    try{
+      if (storedRecipes !== null){
+        setRecipes(JSON.parse(storedRecipes));
+      }
+    }catch (e){
+      console.error("Falha ao carregar receitas.",e);
+    }
+  };
+  loadRecepies();
+ },[]);
+ if(!title){
+  return;
+ }
+ const newRecipe ={
+  id: Date.now().toString(),
+  igredients:igredients,
+ };
+ setRecipes(currentRecipes=>[...currentRecipes ,newRecipe]);
+ setTitle("");
+ setIgredients("");
+ setView('Lista');
+
+const handleDeleteRecipe =(id)=>{
+  setRecipes(currentRecipes=>currentRecipes.filter(recipe=>recipe.id!==id));
+};
+return(
+    <SafeAreaView>
+     <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <Text style={styles.header}>meu livro de receitas </Text>
+      {view === 'lista'?(
+        <View>
+          <TouchableOpacity style={styles.addButton} onPress={()=>setView('formulario')}>
+            <Text style ={styles.buttonText}>Adicionar nova receita </Text>
+          </TouchableOpacity>
+        </View>
+      ): (
+        <View></View>
+      )}
+     </ScrollView>
+    </SafeAreaView>
   );
-}
+
+};
+
 
 const styles = StyleSheet.create({
   container: {
